1258de459e1ac54b577e38561a85baea92d41b56
TBWTK <ilyapim18@gmail.com>
2022-05-17 07:53:28 +0300

    update Caatalog

--- View/MainView/Catalog/CatalogUserControl.xaml.cs
+++ View/MainView/Catalog/CatalogUserControl.xaml.cs
@@ -12,17 +12,84 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CarSales.Model;
 
 namespace CarSales.View.MainView.Catalog
 {
-    /// <summary>
-    /// Логика взаимодействия для CatalogUserControl.xaml
-    /// </summary>
     public partial class CatalogUserControl : UserControl
     {
+        List<Advertisements> advertisements;
+        List<Cars> cars;
+
         public CatalogUserControl()
         {
+            advertisements = new List<Advertisements>();
+            cars = new List<Cars>();
+
             InitializeComponent();
+            using(var context = new CarSalesEntities())
+            {
+                advertisements = context.Advertisements.ToList();
+                cars = context.Cars.ToList();
+                // Заполнение комбобоксов для фильтрации
+
+                ModelsComboBox.ItemsSource = context.Models.ToList();
+                MarksComboBox.ItemsSource = context.Marks.ToList();
+                ColorsComboBox.ItemsSource = context.Colors.ToList();
+                TransmissionsComboBox.ItemsSource = context.Transmissions.ToList();
+                HandlebarsComboBox.ItemsSource = context.Handlebars.ToList();
+                WheeldrivesComboBox.ItemsSource = context.Wheeldrives.ToList();
+                BodyworksComboBox.ItemsSource = context.Bodyworks.ToList();
+            }
+            CatalogListBox.ItemsSource = advertisements;
+        }
+
+        private void MarksComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedIndex = MarksComboBox.SelectedIndex;
+            if (selectedIndex > 0)
+            {
+                Marks mark = (Marks)MarksComboBox.SelectedItem;
+                using (var context = new CarSalesEntities())
+                {
+                    ModelsComboBox.ItemsSource = context.Models.Where(m => m.Mark == mark.Id).ToList();
+                }
+            }
+        }
+
+        private void ModelsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedIndex = MarksComboBox.SelectedIndex;
+            if (selectedIndex > 0)
+            {
+                Marks mark = (Marks)MarksComboBox.SelectedItem;
+                cars = cars.Where(c => c.MarkModel == mark.Id).ToList();
+            }
+        }
+
+        private void ColorsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TransmissionsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void HandlebarsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void WheeldrivesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BodyworksComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
